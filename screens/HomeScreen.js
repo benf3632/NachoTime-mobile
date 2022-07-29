75f8d0084ed09ff9dbe465c4084efed82fdac129
benf3632 <benf3632@gmail.com>
2022-07-29 22:04:19 +0300

    Add app title and icon

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,26 +1,22 @@
 import React, { useState } from "react";
-import {
-  View,
-  Button,
-  StyleSheet,
-  FlatList,
-  Modal,
-  ScrollView,
-  Image,
-} from "react-native";
-import WebView from "react-native-webview";
+import { View, Text, StyleSheet, FlatList, Image } from "react-native";
 import { useSelector, useDispatch } from "react-redux";
 import tw from "tailwind-react-native-classnames";
 
 // components
 import DetailsCard from "../components/DetailsCard";
 import DetailsModal from "../components/DetailsModal";
+import colors from "../constants/colors";
 
 // actions
 import { incrementPage, setPage, addMovies } from "../slices/moviesSlice";
 
 // selectors
 import { selectPage, selectMovies } from "../slices/moviesSlice";
+
+// assets
+import nacho from "../assets/nacho.png";
+import SvgUri from "react-native-svg-uri";
 
 const HomeScreen = () => {
   const page = useSelector(selectPage);
@@ -37,6 +33,10 @@ const HomeScreen = () => {
 
   return (
     <View style={styles.screen}>
+      <View style={styles.appTitleContainer}>
+        <Text style={styles.appTitle}>NachoTime</Text>
+        <Image style={styles.appIcon} source={nacho} />
+      </View>
       {currentDetails && (
         <DetailsModal
           closeModalCallback={() => setModalVisibile(false)}
@@ -62,6 +62,19 @@ const HomeScreen = () => {
 
 const styles = StyleSheet.create({
   screen: { width: "100%", height: "100%", backgroundColor: "#181826" },
+  appTitleContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  appTitle: {
+    fontSize: 32,
+    color: colors.primary,
+  },
+  appIcon: {
+    width: 50,
+    height: 50,
+  },
 });
 
 export default HomeScreen;
