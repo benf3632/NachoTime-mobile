0baa5867dd66fd460a4ec5ed2baa0c33c95e3673
benf3632 <benf3632@gmail.com>
2022-07-30 15:43:56 +0300

    Add fetchMoviesByFilter to yts helper

---
+++ helper/yts.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const baseURL = "https://yts.mx/api/v2";
+
+const ytsAxiosInstance = axios.create({ baseURL: baseURL });
+
+export async function fetchMoviesByFilter(filter, page = 1, limit = 10) {
+  try {
+    const result = await ytsAxiosInstance.get(
+      `list_movies.json?sort_by=${filter}&page=${page}&limit=${limit}`,
+    );
+    if (result.status !== 200) throw new Error("Invalid request");
+    if (result.data.movie_count <= 0) return [];
+    return result.data.movies;
+  } catch (error) {
+    console.error(error);
+    throw new Error(error);
+  }
+}
