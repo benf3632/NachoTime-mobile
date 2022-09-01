f98ea8c3afb5ba9d6cc49682769d48452e619f93
benf3632 <benf3632@gmail.com>
2022-09-01 23:03:56 +0300

    Remove skeleton placeholder and replace Image with FastImage

--- src/components/ShowDetailsCard.js
+++ src/components/ShowDetailsCard.js
@@ -1,38 +1,15 @@
-import React, { memo, useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
-import { View, Text, Image, TouchableOpacity, StyleSheet } from "react-native";
-import SkeletonContent from "react-native-skeleton-content-nonexpo";
-
-// constants
-import colors from "@app/constants/colors";
+import { TouchableOpacity, StyleSheet } from "react-native";
+import FastImage from "react-native-fast-image";
 
 const ShowDetailsCard = ({ onPress, details, style }) => {
-  const [loadingImage, setLoadingImage] = useState(true);
   return (
-    <TouchableOpacity disabled={!!!details} style={style} onPress={onPress}>
-      <Image
-        onLoadEnd={() => {
-          setLoadingImage(false);
-        }}
+    <TouchableOpacity style={style} onPress={onPress}>
+      <FastImage
         style={styles.imageCover}
         source={{ uri: details.large_cover_image }}
       />
-      {loadingImage && (
-        <SkeletonContent
-          boneColor="black"
-          duration={2000}
-          containerStyle={{
-            flex: 1,
-            height: 150,
-            width: 110,
-            position: "absolute",
-            marginRight: 10,
-          }}
-          isLoading={loadingImage}
-          layout={[
-            { key: "imageCover", width: 110, height: 150, marginRight: 10 },
-          ]}></SkeletonContent>
-      )}
     </TouchableOpacity>
   );
 };
@@ -41,7 +18,7 @@ const styles = StyleSheet.create({
   imageCover: {
     width: 110,
     height: 150,
-    resizeMode: "contain",
+    backgroundColor: "black",
   },
 });
 
@@ -50,4 +27,4 @@ ShowDetailsCard.propTypes = {
   details: PropTypes.object,
 };
 
-export default memo(ShowDetailsCard);
+export default ShowDetailsCard;
